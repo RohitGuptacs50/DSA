16cf8b94e47a1bc7fa5284122550ee28c84d2bd9
RohitGuptacs50 <38775212+RohitGuptacs50@users.noreply.github.com>
2021-04-14 22:27:45 +0530

    Create Delete_Node_in_a_BST.py

---
+++ leet code/Delete_Node_in_a_BST.py
@@ -0,0 +1,46 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def MinValue(self, node):
+        current = node
+        
+        while current.left is not None:
+            current = current.left
+        return current
+    
+    def deleteNode(self, root, key):
+        """
+        :type root: TreeNode
+        :type key: int
+        :rtype: TreeNode
+        """
+        if root is None:
+            return root
+        
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+            
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+            
+        else:
+            if root.left is None:
+                temp = root.right
+                root = None
+                return temp
+            
+            elif root.right is None:
+                temp = root.left
+                root = None
+                return temp
+            
+            temp = self.MinValue(root.right)
+            
+            root.val = temp.val
+            root.right = self.deleteNode(root.right, temp.val)
+            
+        return root
