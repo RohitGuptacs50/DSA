b53c713523d271334435d4fb957f1d89c80a966c
RohitGuptacs50 <38775212+RohitGuptacs50@users.noreply.github.com>
2021-04-15 09:36:13 +0530

    Create Remove_Element.py

---
+++ leet code/Remove_Element.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        nums[:] = (a for a in nums if a != val)
+        
+        return len(nums)
