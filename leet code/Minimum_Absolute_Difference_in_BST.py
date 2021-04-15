556f5449a49d221fa146b14de03e9b89c91f308f
RohitGuptacs50 <38775212+RohitGuptacs50@users.noreply.github.com>
2021-04-15 09:23:13 +0530

    Create Minimum_Absolute_Difference_in_BST.py

---
+++ leet code/Minimum_Absolute_Difference_in_BST.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    
+    def inorder(self, node, res):
+        if node:
+            self.inorder(node.left, res)
+            res.append(node.val)
+            self.inorder(node.right, res)
+            
+        else:
+            return 
+    def getMinimumDifference(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        res = []
+        self.inorder(root, res)
+        min = 1000
+        for i in range(len(res) - 1):
+            for j in range(i + 1, len(res)):
+                if min > abs(res[i] - res[j]):
+                    min = abs(res[i] - res[j])
+                    
+        return min
