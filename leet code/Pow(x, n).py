fc5df770b3351cbeb1610f2831270bc1af605109
RohitGuptacs50 <38775212+RohitGuptacs50@users.noreply.github.com>
2021-04-15 09:38:59 +0530

    Create Pow(x, n).py

---
+++ leet code/Pow(x, n).py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        
+        
+        
+        y = x**n
+        return y
+        
