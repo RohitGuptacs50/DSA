d43532882c3d32d5dedab47585fb928b4d19a690
RohitGuptacs50 <38775212+RohitGuptacs50@users.noreply.github.com>
2021-04-15 09:34:34 +0530

    Create Length_of_Last_Word.py

---
+++ leet code/Length_of_Last_Word.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
+        word = s.split()
+        if len(word) == 0:
+            return 0
+        else:
+            return len(word[-1])
